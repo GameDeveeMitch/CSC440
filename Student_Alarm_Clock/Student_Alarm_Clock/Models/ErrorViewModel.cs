3e968d34e140c50fdd58a2f5e8eb5c8a91e7a4a8
Mitchell Hewitt <hewittm2@gmatc.matc.edu>
2020-09-04 15:28:49 -0500

    Blank Template
    This is the initial upload of the project.

---
+++ Student_Alarm_Clock/Student_Alarm_Clock/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Student_Alarm_Clock.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
