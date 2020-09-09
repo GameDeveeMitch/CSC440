dc81c357465716bbf30a8c0c0465c0de1ae0eabb
Mitchell Hewitt <hewittm2@gmatc.matc.edu>
2020-09-09 12:42:25 -0500

    BIG UPDATE
    Sorry again, but this is the fix where the project has been changed from .NET Core to .Net Framework.

---
+++ Student_Alarm_Clock/Student_Alarm_Clock/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Student_Alarm_Clock
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
